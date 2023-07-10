b2c39b7452796279f37fe6d78d3aad3f16b06772
Hayes Neuman <neurosie@gmail.com>
2023-07-10 14:03:07 -0400

    Switch to github version of eggbug
    The cargo version is out of date, this fixes an issue with image uploads.

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv()?;
 
     // download image
+    let client = reqwest::Client::builder()
+        .user_agent(format!(
+            "primidi cohost bot ({})",
+            &env::var("COHOST_EMAIL")?
+        ))
+        .build()?;
     let image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Crocus_Sativus_-_Zaffrano_domestico_-_Saffron._%28saffron_crocus%3B_field_crocus%29_%28NYPL_b14444147-1130719%29.tiff/lossy-page1-1521px-Crocus_Sativus_-_Zaffrano_domestico_-_Saffron._%28saffron_crocus%3B_field_crocus%29_%28NYPL_b14444147-1130719%29.tiff.jpg";
 
-    let response = reqwest::get(image_url).await?;
+    let response = client.get(image_url).send().await?;
     assert_eq!(response.status(), reqwest::StatusCode::OK);
 
     let image_data = response.bytes().await?;
@@ -37,6 +43,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
             image_data,
             "image.jpg".into(),
             "image/jpeg".into(),
+            /* width */ None,
+            /* height */ None,
         )
         .with_alt_text(alt_text)],
         draft: true,
